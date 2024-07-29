6063d823968773175bd0c03e62f4681adbb65311
Edward Andres Quintero Palma <140639492+QuinteroEdward@users.noreply.github.com>
2024-07-29 15:10:46 -0500

    Se agregan los ficheros 1,2,3

---
+++ Cap01/Quintero00.c
@@ -0,0 +1,13 @@
+/*Autor: E. Quintero
+Fecha: 23/07/2024
+Materia: Sistemas Operativos
+Tema: Programación en C : Primera parte*/
+
+#include <stdio.h>
+
+int main (int argc, char *argv[]){
+  
+  printf("Hola Mundo........\n");
+  printf("\t\t Bienvenidos a mi clase de Sistemas Operativos :) \n");
+  return 0; 
+}
