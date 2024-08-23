17ed857fb69dbf0e3a90965279071d582ab799b0
Edward Andres Quintero Palma <140639492+QuinteroEdward@users.noreply.github.com>
2024-08-23 09:30:09 -0500

    Update memAlloc00_Quintero.c

--- Cap01/Gestion de Memoria/memAlloc00_Quintero.c
+++ Cap01/Gestion de Memoria/memAlloc00_Quintero.c
@@ -3,11 +3,11 @@
 #                                         Fecha: 13/08/2024                                  #                                                                                            
 #                                    Materia: Sistemas Operativos                            #                                                                                            
 #                                      Tema: Gestión de memoria                              #                                                                                            
-#                         Tópico: Reasignación de memoria con la función rmalloc()           #                                                                                            
+#                         Tópico: Reserva de memoria con la función malloc()                 #                                                                                            
 ##############################################################################################*/
 /*
 ----------------------------------------------------------------------------------------
-la función memalloc() reserva un espacio de memoria de tamaño requerido y 
+la función malloc() reserva un espacio de memoria de tamaño requerido y 
 devuelve un puntero que señala el inicio del espacio de memoria reservado anteriormente.
 se utiliza para reservar dinámicamente un bloque de memoria. Esto significa que, a diferencia de las variables declaradas al inicio de un programa, 
 la memoria asignada con malloc() se reserva en tiempo de ejecución, cuando el programa se está ejecutando.
